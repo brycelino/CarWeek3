202c5415003432e76df6d39939c9f6fe86d4542a
brycelino <brycejeff16@gmail.com>
2022-02-10 21:23:55 -0600

    week3 assignment
    Create a class called "Car" that includes the following attributes: a make (string), model (string), and speed/mph (int).
    
    Your program's "Main" should ask the user to enter the speed limit. The user should then be prompted to enter information for 3 different cars (make, model, and speed).
    
    If a car is under the speed limit, the Console should display "OK" for that car, along with that car's information. If a car is above the speed limit, the program should calculate the number of demerit points. For every 5mph above the speed limit, 1 demerit point should be incurred. Display the car's information, how many demerit points it received, and if more than 10, display "License Suspended"

---
+++ week3String/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace week3String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car(); // declaring car class in main class to use the car class in main
+            Console.WriteLine("Enter the speed limit: " ); // ask the user to enter speed limit
+            int speedLimit = Convert.ToInt32(Console.ReadLine()); // converts speedlimit to int so the user can input numbers 
+            
+            
+
+            Console.WriteLine("\nEnter the make of your car: " ); // ask user to enter the make of the car 
+            c.make = Console.ReadLine(); // allows user to enter make of their car
+
+            Console.WriteLine("\nEnter the model of your car: "); // asks user to enter the model of their car
+            c.model = Console.ReadLine(); // allows the user to enter the model of their car 
+
+            Console.WriteLine("\nEnter the mph you're currently going: "); // asks user to enter the mph they're going 
+            c.speed = Convert.ToInt32(Console.ReadLine()); // converts speed to int allows the user to input the speed they're going
+            c.demerit = (c.speed - speedLimit) / 5; // calculate the total number of demerits 
+
+            if (c.speed <= speedLimit) // compares the speed to speed limit if under the speed limit they're ok
+                Console.WriteLine("\n" + c.make + " " + c.model + " " + c.speed + "mph: " + "OK");
+            else if (c.demerit < 10 && speedLimit < c.speed) // determines if user has demerits and compares speed limit to user speed
+            {
+
+                Console.WriteLine("\n" + c.make + " " + c.model + " " + c.speed + "mph: " + c.demerit + " demerits ");
+
+            }
+            else
+               if (c.demerit > 10 && speedLimit < c.speed) // determines if the user has over 10 demerits if so license suspended and compares user speed to speed limit, 
+                    Console.WriteLine("\n" + c.make + " " + c.model + " " + c.speed + "mph: " + c.demerit + " demerits " + "<LICENSE SUSPENDED>");
+        }
+    }
+    public class Car // car class stores attributes of car 
+    {
+        public string make; // string for make of the car
+        public string model; // string for model of the car
+        public int speed; // int for speed the user car is going 
+        public int demerit; // int for the demerits
+    }
+}
